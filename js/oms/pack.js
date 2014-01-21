0078d5ab972e98fc34e34bbf27a52b8fa3980f10
Daniele Ciminieri <stonecoldcrazy@gmail.com>
2014-01-21 18:49:10 +0100

    added packed

---
+++ js/oms/pack.js
@@ -0,0 +1,152 @@
+(function(){
+
+  var who = window.who || (window.who = {});
+
+  who.packed = function(){
+
+    
+        var graphWidth = 1200,
+ 		graphHeight = 600,
+	 	transitionDuration = 750,
+	 	fontSize = 12,
+	 	depth=3,
+	 	fontFamily = 'Arial',
+	 	pack;
+	 	
+	 	var rebindData;
+
+
+    function packed(selection){
+    	selection.each(function(data){
+
+        	var chart = selection
+        	var finData={"key":"data",
+        	"values":data}
+        	
+        	
+        	var diameter = 900,
+		    format = d3.format(",d");
+		
+			pack = d3.layout.pack()
+			.children(function(d) {return d.values})
+		    .size([diameter - 4, diameter - 4])
+		    .padding(1)
+		    .value(function(d) { return 1; });
+		    
+		    
+		   var node = chart.datum(finData).selectAll(".node")
+	       .data(pack.nodes)
+	       //.data(function(d) )
+	       
+	        node.enter().append("g")
+	       .attr("class", function(d) { return d.values ? "node" : "leaf node"; })
+	       .attr("depth", function(d) { return d.depth; })
+	       //.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })	
+			
+			
+			circles=node.filter(function(d){if(d.depth>depth) return false; else return true})
+			.append("circle")
+      		//.attr("r", function(d) { return d.r; })
+      		.style("opacity",function(d){if (d.depth==3) return 1; else return 0.25})
+      		.style("fill",function(d){if (d.depth==3) return "#ff7f0e"; else return "#C6DBEB"})
+      		
+      		.attr("cx", function(d) { return d.x; })
+		    .attr("cy", function(d) { return d.y; })
+		    .attr("r", function(d) { return d.r; });	
+      		
+      		
+      		$(".node:first-child").remove();
+      		//node.filter(function(d){return d.depth<=depth})
+      		
+      		node.exit().remove();
+      		
+      		
+      		 dataByTime = function() {
+      		 	
+      			pack = d3.layout.pack()
+				.children(function(d) {return d.values})
+			    .size([diameter - 4, diameter - 4])
+			    .padding(1)
+			    .value(function(d) {return d.time; });
+			    
+			    pack.nodes(finData);
+
+		  
+		    circles.transition()
+		        .duration(500)
+		        .attr("cx", function(d) { return d.x; })
+		    .attr("cy", function(d) { return d.y; })
+		        .attr("r", function(d) { return d.r; });
+      		}
+      		
+      		 defaultData = function() {
+      		 	
+      			pack = d3.layout.pack()
+				.children(function(d) {return d.values})
+			    .size([diameter - 4, diameter - 4])
+			    .padding(1)
+			    .value(function(d) {return 1; });
+			    
+			    pack.nodes(finData);
+
+		  
+		    circles.transition()
+		        .duration(500)
+		        .attr("cx", function(d) { return d.x; })
+		    .attr("cy", function(d) { return d.y; })
+		        .attr("r", function(d) {return d.r; });
+      		}
+      		
+      		deep = function(x) {
+      			depth=x;
+      			
+      		circles.transition().filter(function(d){if(d.depth>depth) return false; else return true})
+      		.style("opacity",function(d){console.log(d); if (d.depth>2) return 1; else return 0.25});
+		    
+  			circles.transition().filter(function(d){if(d.depth>depth) return true; else return false})
+		    .style("opacity",0);
+      		
+      		}
+      		
+      		
+      		
+	
+    	}); //end selection
+    } // end packed
+
+    packed.graphWidth = function(x){
+      if (!arguments.length) return graphWidth;
+      graphWidth = x;
+      return packed;
+    }
+
+    packed.graphHeight = function(x){
+      if (!arguments.length) return graphHeight;
+      graphHeight = x;
+      return packed;
+    }
+    
+    packed.deep = function(x){
+      
+      deep(x);
+      return packed;
+    }
+    
+    packed.bindByTime = function(){
+      dataByTime();
+      
+      return packed;
+    }
+    
+    packed.defaultData = function(){
+      defaultData();
+      
+      return packed;
+    }
+    
+    
+
+    return packed;
+  }
+  
+})();
