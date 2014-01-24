f99272d5e7d34ee41f3c1b8313a9ece81ffcf33d
uf0 <scnepa222@gmail.com>
2014-01-24 10:24:12 +0100

    improved analytics

--- js/analytics_app.js
+++ js/analytics_app.js
@@ -16,30 +16,57 @@ d3.csv('data/cs_analytics.csv', function(data) {
 	urls = tld.group();
 
 	var width = 500,
-		height = 100,
-		tagContainer = d3.select("#category")
+		height = 100;
+
+	var tagContainer = d3.select("#category")
 						.append("svg")
 	     					.attr("width", width)
-	     					.attr("height", height),
-	    domainContainer = d3.select("#domain")
+	     					.attr("height", height)
+	     					.on("click", function(){
+	     						domainContainer.call(domainChart)
+	     						queryContainer.call(queryChart)
+	     					})
+
+	var domainContainer = d3.select("#domain")
 						.append("svg")
 	     					.attr("width", width)
-	     					.attr("height", height*5);
+	     					.attr("height", height*5)
+	     					.on("click", function(){
+	     						tagContainer.call(tagChart)
+	     						queryContainer.call(queryChart)
+	     					})
+
+	var queryContainer = d3.select("#query")
+						.append("svg")
+	     					.attr("width", width)
+	     					.attr("height", width)
+	     					.on("click", function(){
+	     						tagContainer.call(tagChart)
+	     						domainContainer.call(domainChart)
+	     					})
 
     var tagChart = who.barChart()
     				.height(height)
     				.width(width)
+    				.xMax(tags.top(1)[0].value)
     				.dimension(tag)
     				.group(tags)
 
     var domainChart = who.barChart()
     				.height(height*6)
     				.width(width)
+    				.xMax(tlds.top(1)[0].value)
     				.dimension(tld)
     				.group(tlds)
 
-    tagContainer.call(tagChart)
+    var queryChart = who.bubbleChart()
+				.diameter(width)
+				.xMax(queries.top(1)[0].value)
+				.dimension(query)
+				.group(queries)
 
+    tagContainer.call(tagChart)
     domainContainer.call(domainChart)
+    queryContainer.call(queryChart)
 
 });
