4798bde42fb8078356789979bd58fefe426d4532
gianni <scnepa222@gmail.com>
2014-02-26 12:22:51 +0100

    added libs

--- app/js/app.js
+++ app/js/app.js
@@ -2,12 +2,13 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('who', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'ui.bootstrap',
+  'who.filters',
+  'who.services',
+  'who.directives',
+  'who.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
