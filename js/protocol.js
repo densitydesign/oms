94781dfe35ba0dd4621d1da4754a565f319a6e7d
giorgiouboldi <giorgio.uboldi@gmail.com>
2014-03-03 18:04:47 +0100

    Added Protocol

--- js/protocol.js
+++ js/protocol.js
@@ -11,19 +11,24 @@ $('.carousel').carousel({
   "interval":false
 })
 
-$("body").on('click',"#protocol",function() {
+$("body").on('click',"#proto-title-container",function() {
   console.log($("#protocol").css("right"))
   if($("#protocol").css("right") != '0px'){ 
+    //$(".proto-title").addClass(".proto-fixed")
     $("#protocol").css("right",0);
     $(".close-btn").css('-webkit-transform','rotate(90deg)');
     $(".close-btn").css('-moz-transform','rotate(90deg)');
+    $("#proto-title-container").css("right","360px")
   }
    else if($("#protocol").css("right")=="0px") {
+   // $(".proto-title").removeClass(".proto-fixed")
     $("#protocol").css("right","-360px");
     $(".close-btn").css('-webkit-transform','rotate(45deg)');
     $(".close-btn").css('-moz-transform','rotate(45deg)');
+    $("#proto-title-container").css("right","0")
   }
 })
+
 
 
 $("b").hover(function() {
